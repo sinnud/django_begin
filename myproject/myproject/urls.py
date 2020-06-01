9b1941545300856501aef2e36c3986d57b4f9238
Luke Du <sinnud@gmail.com>
2020-06-01 10:29:40 -0700

    Part 1 hello world

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -12,10 +12,20 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from boards import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
 ]
